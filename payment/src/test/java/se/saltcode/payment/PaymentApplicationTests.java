b8098fc70805ac7e625d495fc6e0c3abcc8e26c0
adamhedin <adam.s.hedin@gmail.com>
2024-09-04 13:44:41 +0200

    initialize file structure

---
+++ payment/src/test/java/se/saltcode/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.saltcode.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ shipping/src/test/java/se/saltcode/shipping/ShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.saltcode.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
