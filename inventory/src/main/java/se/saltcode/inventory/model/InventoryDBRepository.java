7ba0ee04fe90d659a9bb36e0bf872891193d4fa6
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-09 15:08:00 +0200

    refactored inventoryDBRepository to JPA instead of ListCrud

--- inventory/src/main/java/se/saltcode/inventory/model/InventoryDBRepository.java
+++ inventory/src/main/java/se/saltcode/inventory/model/InventoryDBRepository.java
@@ -1,11 +1,11 @@
 package se.saltcode.inventory.model;
 
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface InventoryDBRepository extends ListCrudRepository<Inventory, UUID> {
-
+public interface InventoryDBRepository extends JpaRepository<Inventory, UUID> {
+    // refactored to JPA does ListCrud but more
 }
