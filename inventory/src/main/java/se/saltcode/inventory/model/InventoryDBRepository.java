01e37de192444bf498a2a712132c21d2a759f4b4
haraldborestam <harald.borestam@gmail.com>
2024-09-10 13:26:49 +0200

    Add: testing of repository crud functionality and custom findByProduct method in repository class

--- inventory/src/main/java/se/saltcode/inventory/model/InventoryDBRepository.java
+++ inventory/src/main/java/se/saltcode/inventory/model/InventoryDBRepository.java
@@ -3,9 +3,13 @@ package se.saltcode.inventory.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface InventoryDBRepository extends JpaRepository<Inventory, UUID> {
     // refactored to JPA does ListCrud but more
+
+    // Custom method to get item by its product name
+    List<Inventory> findByProduct(String Product);
 }
