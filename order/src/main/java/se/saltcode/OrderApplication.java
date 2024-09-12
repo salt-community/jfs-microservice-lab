4d00c5f756bbd2da1f4e30d2be6e94e6a24ddbb6
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-09-12 17:39:25 +0200

    refactor

--- order/src/main/java/se/saltcode/OrderApplication.java
+++ order/src/main/java/se/saltcode/OrderApplication.java
@@ -11,35 +11,3 @@ public class OrderApplication {
 	}
 
 }
-/*
-api:
-  base-path: /api
-  controllers:
-    orders: /order
-    transactions: /transaction
-server:
-  port: 8080
-spring:
-  config:
-    import: optional:file:.env[.properties]
-  application:
-    name: order
-  datasource:
-    username: ${DATABASE_USERNAME}
-    password: ${DATABASE_PASSWORD}
-  jpa:
-    database-platform: org.hibernate.dialect.PostgreSQLDialect
-    hibernate:
-      ddl-auto: create-drop  #create-drop, create, validate etc
-    show-sql: true  #Set to true to enable SQL logging
-  cloud:
-    gcp:
-      sql:
-        instance-connection-name: "${INSTANCE_CONNECTION_NAME}"
-        database-name: ${DATABASE_NAME}
-      credentials:
-        location: ${GCP_AUTH_LOCATION}
-
-
-
- */
