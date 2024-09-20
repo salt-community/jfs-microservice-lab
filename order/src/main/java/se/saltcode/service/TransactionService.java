c000e0180fb674097965feab44572f976f7390c3
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-09-20 12:48:08 +0200

    removed unescessary transaction repo

--- order/src/main/java/se/saltcode/service/TransactionService.java
+++ order/src/main/java/se/saltcode/service/TransactionService.java
@@ -5,29 +5,29 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import se.saltcode.model.enums.Event;
 import se.saltcode.model.transaction.Transaction;
-import se.saltcode.repository.TransactionRepository;
+import se.saltcode.repository.TransactionDbRepository;
 
 @Service
 public class TransactionService {
 
-  private final TransactionRepository repository;
+  private final TransactionDbRepository transactionService;
 
-  public TransactionService(TransactionRepository repository) {
-    this.repository = repository;
+  public TransactionService(TransactionDbRepository transactionService) {
+    this.transactionService = transactionService;
   }
 
   public List<Transaction> getAllTransactions() {
-    return repository.findAll();
+    return transactionService.findAll();
   }
 
   public Transaction getTransactionById(UUID eventID) {
-    return repository
+    return transactionService
         .findById(eventID)
         .orElseThrow(() -> new IllegalArgumentException("Transaction not found"));
   }
 
   public Transaction createTransaction(Transaction transaction) {
-    return repository.save(transaction);
+    return transactionService.save(transaction);
   }
 
   public Transaction updateTransaction(UUID eventID,   Event eventType, UUID orderId, UUID inventoryId, int change) {
@@ -36,10 +36,10 @@ public class TransactionService {
     transaction.setOrderId(orderId);
     transaction.setInventoryId(inventoryId);
     transaction.setChange(change);
-    return repository.save(transaction);
+    return transactionService.save(transaction);
   }
 
   public void deleteTransaction(UUID eventID) {
-    repository.deleteById(eventID);
+    transactionService.deleteById(eventID);
   }
 }
