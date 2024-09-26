8c99a352db5a0944f6450bd39cf68c78795ae557
Nelson Janusson <nelson.janusson@appliedTechnology.se>
2024-09-26 17:54:46 +0200

    fixed GlobalExceptionHandlerTests

--- inventory/src/test/java/se/saltcode/inventory/exception/GlobalExceptionHandlerTest.java
+++ inventory/src/test/java/se/saltcode/inventory/exception/GlobalExceptionHandlerTest.java
@@ -10,9 +10,14 @@ import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import se.saltcode.inventory.model.cache.OrderCache;
 import se.saltcode.inventory.service.InventoryService;
+import se.saltcode.inventory.service.OrderCacheService;
 
 @EnableRuleMigrationSupport
 @WebMvcTest
@@ -21,6 +26,7 @@ class GlobalExceptionHandlerTest {
   @Autowired private MockMvc mockMvc;
 
   @MockBean private InventoryService inventoryService;
+  @MockBean private OrderCacheService orderCacheService;
 
   // Test to handle IllegalArgumentException thrown by the service, ensuring a 400 Bad Request
   // status
